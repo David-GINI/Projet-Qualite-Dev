d6332562e7a55d3d8111641ada810140a54a64ce
David Gini <dadlab.irl@gmail.com>
2022-11-17 16:25:00 +0100

    Ajout de la classe Commande et des classes Enum associées.

---
+++ src/Viandes.java
@@ -0,0 +1,14 @@
+public enum Viandes {
+    BOEUF("Boeuf"),
+    POULET("Poulet"),
+    POISSON("Poisson"),
+    VEGETARIEN("Végétarien"),
+    BOEUF_HALAL("Boeuf Halal"),
+    BOEUF_CASHER("Boeuf Casher");
+
+    private final String value;
+
+    Viandes(String value) {
+        this.value = value;
+    }
+}
