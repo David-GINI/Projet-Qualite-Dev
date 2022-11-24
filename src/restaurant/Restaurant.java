b42dcdd6a163082092db81a2ecd7f54f70d01e87
David Gini <dadlab.irl@gmail.com>
2022-11-24 14:34:43 +0100

    Ajout de fonctionnalités à la classe Restaurant

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -1,12 +1,25 @@
 package restaurant;
 import client.Client;
+import commande.Commande;
+import employe.Employe;
+import table.Table;
+
 import java.util.ArrayList;
 
 public class Restaurant {
     public EtatProprete etatProprete;
-    public ArrayList<Client> listclient = new ArrayList<>();
-    public float caisse;
+    public ArrayList<Employe> listeEmployes = new ArrayList<Employe>();
+    public ArrayList<Client> listeClients = new ArrayList<Client>();
+    public ArrayList<Commande> listeCommandes = new ArrayList<Commande>();
+    public ArrayList<Table> listeTables = new ArrayList<Table>();
+    public double caisse;
 
+    public Restaurant(EtatProprete etatProprete, float caisse) {
+        this.etatProprete = EtatProprete.PROPRE;
+        this.caisse = 0;
+    }
 
-
+    public void genereClient() {
+        
+    }
 }
